eb36525ca49bedce21a86d713fcf572ff41f0168
davidecarlson <david.carlson@stonybrook.edu>
2025-03-24 11:32:01 -0400

    update mouse reference path

--- job_scripts/0.make_index.sh
+++ job_scripts/0.make_index.sh
@@ -2,8 +2,8 @@
 
 module load rna_seq/1.0
 
-GENOME=/gpfs/projects/GenomicsCore/indexes/mouse_gencode_m29/GRCm39.primary_assembly.genome.fa
-GTF=/gpfs/projects/GenomicsCore/indexes/mouse_gencode_m29/gencode.vM29.annotation.gtf
+GENOME=/gpfs/projects/GenomicsCore/indexes/mouse_gencode_m35/GRCm39.primary_assembly.genome.fa
+GTF=/gpfs/projects/GenomicsCore/indexes/mouse_gencode_m35/gencode.vM35.primary_assembly.annotation.gtf
 OUTDIR=index
 
 mkdir -p ${OUTDIR}
