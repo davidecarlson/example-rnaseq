51aaa1dfc2d4af81ee2fa7601018ed10e9d2d674
davidecarlson <david.carlson@stonybrook.edu>
2025-03-24 11:37:39 -0400

    reduce number of reads that are sampled

--- job_scripts/1.subsample.sh
+++ job_scripts/1.subsample.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#subsample reads to keep only 10% per sample
+#subsample reads to keep only 1 million PE reads per sample
 
 READDIR=/gpfs/projects/GenomicsCore/BGE510/reads
 OUTDIR=subsample
@@ -11,8 +11,8 @@ module load hts/1.0
 
 for i in `ls ${READDIR}/*_1.fastq.gz`; do
 	NAME=`basename $i _1.fastq.gz`;
-	seqtk sample -s100 ${READDIR}/${NAME}_1.fastq.gz 5000000 | gzip > ${OUTDIR}/${NAME}_subsample_1.fastq.gz;
-	seqtk sample -s100 ${READDIR}/${NAME}_2.fastq.gz 5000000 | gzip > ${OUTDIR}/${NAME}_subsample_2.fastq.gz;
+	seqtk sample -s100 ${READDIR}/${NAME}_1.fastq.gz 1000000 | gzip > ${OUTDIR}/${NAME}_subsample_1.fastq.gz;
+	seqtk sample -s100 ${READDIR}/${NAME}_2.fastq.gz 1000000 | gzip > ${OUTDIR}/${NAME}_subsample_2.fastq.gz;
 done
 
 
