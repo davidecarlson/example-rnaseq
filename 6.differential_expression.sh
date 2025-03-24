c6e84f4054ee4ce67de30687e6ca8aa41785ff59
davidecarlson <david.carlson@stonybrook.edu>
2025-03-24 11:54:26 -0400

    remove redundant scripts

--- 6.differential_expression.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-module unload hts
-module load deseq2/1.38.0
-
-Rscript deseq2.R
-
-
--- 7.multiqc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-module load hts
-
-multiqc . --outdir multiqc_report
-
